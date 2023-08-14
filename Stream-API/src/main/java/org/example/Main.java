230561a5e07dba77b53e145fe24bb919f2ebd7c1
alireza gholami <gholamialireza1004@gmail.com>
2023-08-14 10:10:34 +0330

    Complete the stream api

--- Stream-API/src/main/java/org/example/Main.java
+++ Stream-API/src/main/java/org/example/Main.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         TextAnalyzer textAnalyzer = new TextAnalyzer();
+        System.out.println(textAnalyzer.numLines());
+        System.out.println(textAnalyzer.numWordLines());
         System.out.println(textAnalyzer.wordRepeat().get("end"));
+        System.out.println(textAnalyzer.numWordSentences().get(2));
+        System.out.println(textAnalyzer.WordsInLines().get("really"));
+        System.out.println(textAnalyzer.adjustedWordCount());
+        System.out.println(textAnalyzer.sentencesMappedByFirstWord().get("Adelaïda"));
+        System.out.println(textAnalyzer.averageCharactersPerSentence());
     }
 }
--- Stream-API/src/main/java/org/example/TextAnalyzer.java
+++ Stream-API/src/main/java/org/example/TextAnalyzer.java
@@ -6,13 +6,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TextAnalyzer {
     public Path path = Paths.get("C:\\Users\\ALIREZA\\Documents\\GitHub\\Ham-Masir\\Stream-API\\src\\main\\java\\org\\example\\TheBrothersKaramazov.txt");
     public static List<String> lines = new ArrayList<>();
     public  Path pathStop = Paths.get("C:\\Users\\ALIREZA\\Documents\\GitHub\\Ham-Masir\\Stream-API\\src\\main\\java\\org\\example\\StopWords.txt");
-    public static List<String> stopWords = new ArrayList<>();
-    public static List<String> sentences = new ArrayList<>();
+    public  List<String> stopWords = new ArrayList<>();
+    public  List<String> sentences = new ArrayList<>();
     public TextAnalyzer() throws IOException {
         this.lines = Files.lines(Paths.get(path.toUri())).toList();
         this.lines = lines.stream().filter(x -> (!x.isEmpty())).map(line->
@@ -30,7 +31,7 @@ public class TextAnalyzer {
                 .filter(s -> !s.isEmpty())
                 .collect(Collectors.toList());
     }
-    public long numLines(List<String> lines){
+    public long numLines(){
         return lines.stream().count();
     }
     public long numWordLines() throws IOException {
@@ -46,7 +47,36 @@ public class TextAnalyzer {
         Map<String, Long> wordCounts = lines.stream()
                 .flatMap(s -> Arrays.stream(s.split("[.?! ]")))
                 .filter(w -> !stopWords.contains(w.toLowerCase()))
-                .collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()));
+                .collect(Collectors.groupingBy(w->w, Collectors.counting()));
         return wordCounts;
+    }
+
+    public Map<String, List<Integer>> WordsInLines() {
+        return IntStream.range(0, lines.size())
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(i, lines.get(i).split("[.?! ]")))
+                .flatMap(entry -> IntStream.range(0, entry.getValue().length)
+                        .mapToObj(j -> new AbstractMap.SimpleEntry<>(entry.getValue()[j], entry.getKey())))
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+    }
+
+    public double averageCharactersPerSentence(){
+        return sentences.stream()
+                .mapToInt(String::length)
+                .average()
+                .orElse(0);
+    }
+    public long adjustedWordCount(){
+        return sentences.stream()
+                .mapToLong(line -> Arrays.stream(line.split("[.?! ]")).count())
+                .reduce(0,(a,b)->b%2 == 0 ? a+b : a-b);
+    }
+
+    public Map<String,String> sentencesMappedByFirstWord(){
+        return sentences.stream()
+                .collect(Collectors.toMap(
+                        line -> line.split("[.?! ]")[0],
+                        line -> line,
+                        (line1, line2) -> line1
+                ));
     }
 }
