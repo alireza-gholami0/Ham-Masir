df660a1fd512d99d9b7e05589917f67fa0bc9c09
alireza gholami <gholamialireza1004@gmail.com>
2023-08-17 20:52:12 +0330

    add ThreadPool Class

--- Race_Conditon/src/main/java/org/example/Main.java
+++ Race_Conditon/src/main/java/org/example/Main.java
@@ -1,16 +1,13 @@
 package org.example;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 
 public class Main {
     public static void main(String[] args) throws IOException, CsvValidationException {
@@ -22,16 +19,14 @@ public class Main {
             Account account = new Account(Integer.parseInt(record[0]),record[1],Long.parseLong(record[2]));
             accounts.add(account);
         }
-        accounts.sort((x,y)-> (x.getID() > y.getID() ? 1:-1));
+        accounts.sort((x,y)-> (Integer.compare(x.getID(), y.getID())));
         reader = new CSVReader(new FileReader("Transactions.csv"));
-        ExecutorService executer = Executors.newFixedThreadPool(100);
+        ThreadPool threadPool = new ThreadPool(10);
         while ((record = reader.readNext()) != null){
             record[2] = record[2].replaceAll(",","");
-            executer.execute(new Transaction(accounts.get(Integer.parseInt(record[0])-1),accounts.get(Integer.parseInt(record[1])-1),Long.parseLong(record[2])));
+            threadPool.execute(new Transaction(accounts.get(Integer.parseInt(record[0])-1),accounts.get(Integer.parseInt(record[1])-1),Long.parseLong(record[2])));
         }
-        executer.shutdown();
-        if (executer.isShutdown()) {
-            accounts.forEach(x-> System.out.println(x.toString()));
-        }
+        threadPool.shutdown();
+        accounts.forEach(x-> System.out.println(x.toString()));
     }
 }
